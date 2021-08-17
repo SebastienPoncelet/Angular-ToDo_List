8595fb54b060b1a361d3676494200ded228710cb
Sébastien Poncelet <sebastienponcelet@hotmail.com>
2021-08-17 23:35:43 +0800

    Update App testing
    Add task delete test

--- my-todo-list/src/app/app.component.spec.ts
+++ my-todo-list/src/app/app.component.spec.ts
@@ -1,15 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import {TasksService} from './_services/tasks.services';
+import {HttpClientModule} from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+  let Tasks: any;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
+      ],
+      providers: [
+        TasksService
       ],
     }).compileComponents();
   });
@@ -20,16 +30,84 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-todo-list'`, () => {
+  // it('modify a task in the list', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
+
+  it('should remove the second episode from the episodes array', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-todo-list');
-  });
+    Tasks = [
+      {
+        id: 1,
+        userId: 1,
+        title: 'FAKE TASK 1',
+        completed: true
+      },
+      {
+        id: 2,
+        userId: 1,
+        title: 'FAKE TASK 2',
+        completed: false
+      },
+    ];
+    app.items = [
+      {
+        id: 1,
+        userId: 1,
+        title: 'FAKE TASK 1',
+        completed: true
+      },
+      {
+        id: 2,
+        userId: 1,
+        title: 'FAKE TASK 2',
+        completed: false
+      },
+    ];
+    app.deleteTask(0);
+    // Assert
+    expect(app.items).toEqual([Tasks[1]]);
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-todo-list app is running!');
-  });
 });
+
+// describe('AppComponent', () => {
+//   // let component: AppComponent;
+//   let Tasks: any;
+//   // let mockEpisodeService;
+
+//   beforeEach(() => {
+//     Tasks = [
+//       {
+//         title: 'FAKE TASK 1',
+//         completed: true
+//       },
+//       {
+//         title: 'FAKE TASK 2',
+//         completed: false
+//       },
+//     ];
+//   })
+
+//   describe('Delete', () => {
+//       it('should remove the second episode from the episodes array', () => {            
+           
+//           // Arrange
+//           // mockEpisodeService.deleteEpisode.and.returnValue(of(true));            
+//           // component.tasks = EPISODES;
+           
+//           // Act
+//           const fixture = TestBed.createComponent(AppComponent);
+//           const app = fixture.componentInstance;
+//           // let component = TestBed.createComponent(AppComponent);
+//           app.deleteTask(0);
+//           // component.delete(EPISODES[1]);
+           
+//           // Assert
+//           expect(app.items).toEqual(Tasks[1]);  
+//       })       
+//   })
+// })
