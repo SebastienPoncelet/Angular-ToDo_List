f6b6839fff8ea31fdbe6533d593b18d6a90486bc
Sébastien Poncelet <sebastienponcelet@hotmail.com>
2021-08-17 19:45:37 +0800

    Update table with delete
    Update the table to remove a specific task from the list.

--- my-todo-list/src/app/app.component.ts
+++ my-todo-list/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Item } from './item';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import {TasksService} from './_services/tasks.services';
 
+// TODO: Add alert toa sk for confirmation before deleting a task.
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -38,7 +39,6 @@ export class AppComponent {
     localStorage.setItem('test-item', JSON.stringify(this.items))
 
     this.getTasks();
-    // console.log("USER TEMP 47");
   }
 
   public getTasks() {
@@ -48,52 +48,38 @@ export class AppComponent {
         response.slice(0, 11).map((item: any, i: any) => {
           this.items.push(item);
         });
-        console.log('app.component.ts - 48 ==> response', response)
-        // this.items = response;
-        // console.log("yolo", response);
+        console.log('app.component.ts - 50 ==> response', response)
       },
       error => {
+        // TODO: throw exception
         // console.log("yolo-error", error);
       }
     )
 
   }
 
-	/* When input is empty, it will
-	not create a new division */
-	public addToList(newTask: any) {
-    if(newTask.title == '')
+  /* When input is empty, it will
+  not create a new division */
+  createTaskOnSubmit() {
+    if(this.registerForm.value.title == '')
     {}
     else {
-      this.items.push(newTask);
+      console.log('app.component.ts - 66 ==> this.registerForm.value', this.registerForm.value)
+      this.items.push(this.registerForm.value);
     }
-		// if (this.newTask == '') {
-		// }
-		// else {
-		// 	this.items.push(this.newTask);
-		// 	this.newTask = '';
-		// }
-	}
-
-  createTaskOnSubmit() {
-
-    // this.tasksService.create(this.registerForm.value).subscribe(
-    //   response => {
-    //     // console.log("yolo", response);
-    //     // console.log(response['id']);
-    //     this.userID.emit(response['id']); // Emit User ID to child User
-    //     this.redirectUser(response['id']);
-    //   },
-    //   error => {
-    //     // console.log("yolo-error", error);
-    //   });
-
-    this.addToList(this.registerForm.value)
   }
+
+  /* This function takes to input the
+	task, that has to be deleted*/
+	public modifyTask(index: number) {
+    console.log('app.component.ts - 73 ==> index', index)
+		this.items.splice(index, 1);
+	}
 
 	/* This function takes to input the
 	task, that has to be deleted*/
 	public deleteTask(index: number) {
+    console.log('app.component.ts - 73 ==> index', index)
 		this.items.splice(index, 1);
 	}
 }
